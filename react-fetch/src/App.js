87a42e39dc5178cd7a98d76e3d66a668b22b5078
Obaid Ur Rahman <obaidkhan972@gmail.com>
2021-09-04 17:36:30 +0500

    useEffect hook

--- react-fetch/src/App.js
+++ react-fetch/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  let url = 'https://jsonplaceholder.typicode.com/todos/1';
+  let [isData, setData] = useState({ title: 'Waiting for Data' });
+  let [isFetching, setFetching] = useState(false);
+
+  //Defining an asynch function to fetch data
+  const dataFetch = async () => {
+    setFetching(true);
+    let response = await fetch(url);
+    setData(await response.json());
+    setFetching(false);
+  }
+
+  useEffect(() => {
+    dataFetch();
+  },[]) //empty 2nd argument, effect will be executed only on first render
+
+  if (isFetching) {
+    return(
+      <div>
+        <h2>Home</h2>
+        <h3>Loading Data....</h3>
+      </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>HOME</h2>
+      <h3>Fetching Data using useEffect</h3>
+      <h5>Title:  {isData.title}</h5>
     </div>
   );
 }
